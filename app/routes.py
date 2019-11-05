2f7c472879c649a8197d96b7ca70b9ace9037a63
Francesco <fra.arzani@gmail.com>
2019-11-05 17:38:53 +0100

    added server code

--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,46 @@ from cqc.pythonLib import CQCConnection, qubit
 @app.route('/index')
 def index():
     return render_template('accueil.html', titre='Accueil')
+
+
+global_stack = ExitStack()
+serverState={}
+Serveur=global_stack.enter_context( CQCconnection('Bob'))
+
+@app.route('/preparationQubit', methods=['POST'])    
+def preparationQubit():
+	if not request.json or not 'theta' in request.json or not 'id' in request.json:
+		abort(400)
+		
+	theta = request.json['theta']
+	idi = request.json['id']
+	
+	qubit = qubit(Serveur)
+	qubit = qubit.ROT_Z(theta)
+	serverState{idi}=qubit
+	
+	
+	return jsonify({'error':False}), 201
+	
+	
+@app.route('/preparationGraphState', methods=['POST'])    
+def preparationGraphState():
+	if not request.json or not 'entanglement_list' in request.json:
+		abort(400)
+	for ((x1,y1),(x2,y2)) in request.json['entanglement_list']:
+		serverState((x1,y1)).CPHASE((x2,y2))
+		
+	return jsonify({'error':False}), 201
+	
+@app.route('/measurementAngle', methods=['POST'])    
+def measureAngle():
+	if not request.json or not 'theta' in request.json or not 'id' in request.json:
+		abort(400)
+		
+	theta = request.json['theta']
+	idi = request.json['id']
+	
+	serverState{idi}.ROT_Z(theta)
+	s=serverState{idi}.measure()
+	
+	return jsonify({'measurement':s}), 201
